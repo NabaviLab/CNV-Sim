327aaab4fa2d2e0f2cc69fad9f43658ba0671e8b
Abdelrahman Hosny <abdelrahman.hosny@hotmail.com>
2016-06-08 16:03:59 -0400

    writing CNV list to a file after sorting it

--- 1. cnv_simulator/CNV-SIM.py
+++ 1. cnv_simulator/CNV-SIM.py
@@ -1,6 +1,18 @@
+#!/usr/bin/python
+
 __author__ = 'Abdelrahman Hosny'
 
+import sys
+import os.path
+import random
+from operator import itemgetter
+
 def readGenome(filename):
+    '''
+    Read a FASTA genome
+    :param filename: genome fasta file name
+    :return: a header string and a genome string
+    '''
     genome = ''
     header = ''
     with open(filename, 'r') as f:
@@ -12,35 +24,98 @@ def readGenome(filename):
                 header = line.strip()
     return header, genome
 
-genome_file = 'data/chr1.fa'
-target_file = 'data/chr1-sample-target.bed'
+def readTargets(filename):
+    '''
+    Read target file in BED format
+    :param filename: target bed file name
+    :return: a list of targets (exons). each target is a dictionary of:
+             chromosome, start, end, description, score, strand
+    '''
+    exons = []
+    with open(filename, 'r') as tf:
+        for line in tf:
+            chromosome, start, end, description, score, strand = line.strip().split('\t')
+            exon = {"chromosome": chromosome, \
+                    "start": int(start), \
+                    "end": int(end), \
+                    "description": description, \
+                    "score": score, \
+                    "strand": strand}
+            exons.append(exon)
+    return exons
 
-modified_genome_file = 'data/chr1.sim.fa'
-modified_target_file = 'data/chr1-sample-target.sim.bed'
+def generateCNVMask(number_of_exons, p_amplify=0.3, p_delete=0.2):
+    '''
+    This function generates random Copy Number Variations mask list
+    :param exons: list of exons.
+    :param p_amplify: percentage of amplifications introduced
+    :param p_delete: percentage of deletions introduced
+    :return: a list of the same length as the exons list. each list item
+             indicates the variation to be added to the exon in the same position.
+             Positive number: number of amplification
+             Zero: no change
+             -1: delete this exon
+    '''
 
-exons = []
-with open(target_file, 'r') as tf:
-    for line in tf:
-        chromosome, start, end, description, score, strand = line.strip().split('\t')
-        exon = {"chromosome": chromosome, \
-                "start": int(start), \
-                "end": int(end), \
-                "description": description, \
-                "score": score, \
-                "strand": strand}
-        exons.append(exon)
+    number_of_amplifications = int(p_amplify * number_of_exons)
+    number_of_deletions = int(p_delete * number_of_exons)
+    cnv_mask = [0] * number_of_exons
 
-# just for testing <adding one amplification only>
-# this portion should be changed with our smart algorithm
-header, genome = readGenome(genome_file)
-last_exon = exons[-1]
-modified_genome = genome[:last_exon["end"]] + genome[last_exon["start"]:last_exon["end"]] + genome[last_exon["end"]:]
-exons[-1]["end"] = last_exon["end"] + (last_exon["end"] - last_exon["start"])
+    # generate CNV mask (a list of amplifications and deletions to be applied to the exons)
+    while number_of_amplifications > 0:
+        choice = random.randrange(0, number_of_exons)
+        while cnv_mask[choice] != 0:
+            choice = random.randrange(0, number_of_exons)
+        cnv_mask[choice] = random.randrange(3,11)     # random amplifications in the range [3, 10]
+        number_of_amplifications -= 1
+    random.shuffle(cnv_mask)
+    while number_of_deletions > 0:
+        choice = random.randrange(0, number_of_exons)
+        while cnv_mask[choice] != 0:
+            choice = random.randrange(0, number_of_exons)
+        cnv_mask[choice] = -1  # random amplifications in the range [3, 10]
+        number_of_deletions -= 1
+    random.shuffle(cnv_mask)
 
-print len(genome)
-print len(modified_genome)
-print len(modified_genome) - len(genome)
+    return cnv_mask
 
+def saveCNVList(exons, cnv_mask, filename):
+    '''
+    Saves the updated exon regions annotated with the amplifications or deletions to be introduced.
+    :param exons: a list of exon objects
+    :param cnv_mask: a list of changes from the function generateCNVMask
+    :return: True when the the write to file process is successful
+    '''
+    with open(filename, 'w') as f:
+        for i in range(0, len(exons)):
+            line = exons[i]["chromosome"] + '\t' \
+                    + str(exons[i]["start"]) + '\t' \
+                    + str(exons[i]["end"]) + '\t' \
+                    + exons[i]["description"] + '\t' \
+                    + exons[i]["score"] + '\t' \
+                    + exons[i]["strand"] + '\t' \
+                    + str(cnv_mask[i]) + '\n'
+            f.write(line)
+    return True
+
+genome_file = sys.argv[1]
+target_file = sys.argv[2]
+simulation_name = sys.argv[3]
+
+genome = readGenome(genome_file)
+exons = readTargets(target_file)
+exons = sorted(exons, key=itemgetter("start"))
+
+
+modified_genome_file = os.path.dirname(os.path.realpath(__file__)) + '/../output/cnvsim_output/'
+modified_target_file = os.path.dirname(os.path.realpath(__file__)) + '/../output/cnvsim_output/'
+cnv_list_file = os.path.dirname(os.path.realpath(__file__)) + '/../output/cnvsim_output/' + simulation_name + '-CNVList.bed'
+
+cnv_mask = generateCNVMask(len(exons))
+saveCNVList(exons, cnv_mask, cnv_list_file)
+
+
+'''
 # now write results to files
 with open(modified_genome_file, 'w') as f:
     f.write(header)
@@ -59,3 +134,4 @@ with open(modified_target_file, 'w') as f:
                  '\t' + ex["strand"] + '\n'
         targets.append(target)
     f.writelines(targets)
+    '''
